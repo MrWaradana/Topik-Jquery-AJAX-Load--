ebda37915b9d9316426e5d58ce306d7b6a59eba1
Muhammad Ridho Waradana <muhammadridhowaradana@gmail.com>
2021-10-17 18:26:38 +0700

    add comments

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 $(document).ready(function(){
     $("#video-btn").click(function(){
-      $("#content").load("embed-videos.html #embed-videos", function(responseTxt, statusTxt, xhr){
+      $("#content1").load("embed-videos.html #embed-videos", function(responseTxt, statusTxt, xhr){
+        if(statusTxt == "success")
+          alert("External content loaded successfully!");
+        if(statusTxt == "error")
+          alert("Error: " + xhr.status + ": " + xhr.statusText);
+      });
+    });
+    $("#comments-btn").click(function(){
+      $("#content2").load("comments.html #comments", function(responseTxt, statusTxt, xhr){
         if(statusTxt == "success")
           alert("External content loaded successfully!");
         if(statusTxt == "error")
